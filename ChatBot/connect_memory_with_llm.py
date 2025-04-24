32f3dc8cecc66e990cdfc895ca84743e7bd95cf0
bitsnwheels <adarshmfp54@gmail.com>
2025-04-24 17:11:41 +0530

    Add files via upload
    My first commit

---
+++ ChatBot/connect_memory_with_llm.py
@@ -0,0 +1,59 @@
+import os  # because we have to load the contents of .env file
+from langchain_huggingface import HuggingFaceEndpoint  # Import HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+from dotenv import load_dotenv
+load_dotenv()  # This loads the variables from the .env file
+
+# Step 1: Setup LLM (Mistral with HuggingFace)
+HF_TOKEN = os.environ.get("HF_TOKEN")
+HUGGINGFACE_REPO_ID =  "mistralai/Mistral-7B-Instruct-v0.3"
+
+
+def load_llm(huggingface_repo_id):
+    llm = HuggingFaceEndpoint(  # Use HuggingFaceEndpoint instead of HuggingFaceHub
+        repo_id=huggingface_repo_id,
+        huggingfacehub_api_token=HF_TOKEN,  # Use correct keyword for HuggingFace API token
+        task="text-generation",
+        temperature = 0.1,
+        model_kwargs={"max_length": 512}
+    )
+    return llm
+
+CUSTOM_PROMPT_TEMPLATE = """
+Use the pieces of information provided in the context to answer user's question.
+If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+Don't provide anything out of the given context.
+
+Context: {context}
+Question: {question}
+
+Start the answer directly. No small talk please.
+"""
+
+def set_custom_prompt(custom_prompt_template):
+    prompt = PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+    return prompt
+
+# Load Database
+DB_FAISS_PATH = "vectorstore/db_faiss"
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db = FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
+
+# Create QA chain
+qa_chain = RetrievalQA.from_chain_type(
+    llm=load_llm(HUGGINGFACE_REPO_ID),
+    chain_type="stuff",
+    retriever=db.as_retriever(search_kwargs={'k': 3}),  # extracts the top 3 results
+    # return_source_documents=True,
+    chain_type_kwargs={'prompt': set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
+)
+
+# Now invoke with a single query
+user_query = input("Write Query Here: ")
+response = qa_chain.invoke({'query': user_query})
+print("RESULT: ", response["result"])
+# print("SOURCE DOCUMENTS: ", response["source_documents"])
---
+++ ChatBot/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+def load_llm(huggingface_repo_id):
+#     llm = HuggingFaceHub(
+#         repo_id=huggingface_repo_id,
+#         model_kwargs={"temperature": 0.5, "max_length": 512},
+#         huggingfacehub_api_token=HF_TOKEN
+#     )
+#     return llm
